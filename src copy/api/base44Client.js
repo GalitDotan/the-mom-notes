5fb0aea1d407b4b26d2ce580c1fba3b7c805e6fc
Galit Dotan <galitdotan99@gmail.com>
2025-05-31 13:06:36 +0300

    Removed coppies

--- src copy/api/base44Client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@base44/sdk';
-// import { getAccessToken } from '@base44/sdk/utils/auth-utils';
-
-// Create a client with authentication required
-export const base44 = createClient({
-  appId: "683aa752ded8787c2f8f8a57", 
-  requiresAuth: true // Ensure authentication is required for all operations
-});
